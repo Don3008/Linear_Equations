1a8069c3362f445fb7ce68e915775267aae90492
Don3008 <don3008@yandex.ru>
2021-07-09 01:49:30 +0300

    Initial commit

---
+++ solver/Main.java
@@ -0,0 +1,28 @@
+package solver;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            LogManager.getLogManager().readConfiguration(
+                    new FileInputStream("logging.properties"));
+        } catch (IOException e) {
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+
+        try (final var input = new Scanner(new File(args[1]));
+             final var output = new PrintWriter(args[3])) {
+            new Application(input, output).run();
+        } catch (FileNotFoundException e) {
+            log.log(Level.SEVERE, "File not found", e);
+        }
+    }
+}
